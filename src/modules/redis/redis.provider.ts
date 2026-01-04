50fd1a5acbde6a4c7b1ea16d9e35cc8b0aeca684
Emmanuel Gutierrez <emma.g2705@gmail.com>
2026-01-04 05:41:53 -0300

    config redis

--- src/modules/redis/redis.provider.ts
+++ src/modules/redis/redis.provider.ts
@@ -5,6 +5,13 @@ export const RedisProvider = {
   provide: 'REDIS',
   inject: [ConfigService],
   useFactory: async (configService: ConfigService) => {
+    console.log({
+      host: configService.get<string>('config.redis.host') as string,
+      username: configService.get<string>('config.redis.username') as string,
+      port: configService.get<number>('config.redis.port') as number,
+      db: configService.get<number>('config.redis.db') as number,
+      password: configService.get<string>('config.redis.password') as string,
+    });
     const client = new Redis({
       host: configService.get<string>('config.redis.host') as string,
       username: configService.get<string>('config.redis.username') as string,
